9420d5cfd7f1351a6f818f305a982a5a74848797
John Absher <jabsher@squarespace.com>
2024-07-08 19:16:46 -0400

    Content Updates

--- astro-v1/src/consts.ts
+++ astro-v1/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "John Absher's Blog";
+export const SITE_DESCRIPTION = 'This website contains links, blog posts, and other material I find interesting and useful.';
