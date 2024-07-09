81d7a406012cc91cfed86f08908092cd0adcf1a7
John Absher <jabsher@squarespace.com>
2024-07-09 11:07:02 -0400

    adding Google Analytics

--- astro-v1/astro.config.mjs
+++ astro-v1/astro.config.mjs
@@ -2,10 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import partytown from '@astrojs/partytown'
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://slabby.xyz',
-  integrations: [mdx(), sitemap(), robotsTxt()]
+  integrations: [mdx(), sitemap(), robotsTxt(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },}),
+    ]
 });
