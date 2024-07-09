9a2fd44fb3b03c25a4400c77c886454b90c77ba1
John Absher <jabsher@squarespace.com>
2024-07-09 10:45:53 -0400

    fixing robots.txt

--- astro-v1/astro.config.mjs
+++ astro-v1/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://slabby.xyz',
-	integrations: [mdx(), sitemap()],
+  site: 'https://slabby.xyz',
+  integrations: [mdx(), sitemap(), robotsTxt()]
 });
